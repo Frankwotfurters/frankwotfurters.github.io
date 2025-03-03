7848dc8d0d4bb04f302f3d2b125edb622546d6b7
Frankwotfurters <59547088+Frankwotfurters@users.noreply.github.com>
2025-03-03 19:37:23 +0800

    MANGA WATCH BASIC FUNC

---
+++ manga_watch/index.js
@@ -0,0 +1,84 @@
+function openModal() {
+    document.getElementById("mangaModal").style.display = "block";
+    document.getElementById("modalOverlay").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("mangaModal").style.display = "none";
+    document.getElementById("modalOverlay").style.display = "none";
+}
+
+function addManga() {
+    let url = document.getElementById("mangaUrl").value;
+    if (url) {
+        let saved_manga = JSON.parse(localStorage.getItem("savedManga")) || {};
+        if (!saved_manga.hasOwnProperty(url)) {
+            fetch(`http://127.0.0.1:5000/import_manga?url=${url}`)
+            .then(response => response.json())
+            .then(data => {
+                saved_manga[url] = data;
+                saved_manga[url]["last_read"] = 0;
+                localStorage.setItem("savedManga", JSON.stringify(saved_manga));
+                updateMangaList();
+            })
+            .catch(error => console.error("Error fetching chapters:", error));
+            alert("Manga URL added: " + url);
+        } else {
+            alert("This URL is already added.");
+        }
+        closeModal();
+    }
+}
+
+function updateMangaList() {
+    let saved_manga = JSON.parse(localStorage.getItem("savedManga")) || [];
+    let mangaContainer = document.getElementById("manga_container");
+    mangaContainer.innerHTML = "";
+
+//     <div class="manga">
+//     <img class="manga_thumbnail"
+//         src="https://images.squarespace-cdn.com/content/v1/6670add926f2a64cd00fb0e7/d2f9b9c1-ab9c-4fe2-a793-d6a8634ac920/character+chii.png">
+//     <h4 class="title">
+//         <u>Chiikawa</u>
+//     </h4>
+// </div>
+
+    Object.keys(saved_manga).forEach(url => {
+        
+        let a = document.createElement("a");
+        let d = document.createElement("div");
+        let unread_d = document.createElement("div");
+        let img = document.createElement("img");
+        let h4 = document.createElement("h4");
+        let u = document.createElement("u");
+        
+        fetch(`http://127.0.0.1:5000/get_last?source=${encodeURIComponent(saved_manga[url]['source'])}&id=${encodeURIComponent(saved_manga[url]['manga_id'])}`)
+        .then(response => response.json())
+        .then(data => {
+            last_read = saved_manga[url]['last_read'];
+            new_chapters = data["last_chapter"] - last_read
+
+            if (new_chapters) {
+                unread_d.innerHTML = new_chapters
+                d.appendChild(unread_d);
+            }
+        })
+        .catch(error => console.error("Error fetching last chapter:", error));
+        
+        a.href = `manga.html?source=${saved_manga[url]['source']}&id=${saved_manga[url]['manga_id']}&url=${url}`
+        d.classList.add("manga");
+        unread_d.classList.add("unread");
+        img.classList.add("manga_thumbnail");
+        img.src = saved_manga[url]["thumbnail_url"];
+        h4.classList.add("title");
+        u.innerHTML = saved_manga[url]["manga_title"]
+        a.appendChild(d);
+        d.appendChild(img);
+        d.appendChild(h4);
+        h4.appendChild(u);
+        mangaContainer.appendChild(a);
+    });
+}
+
+// Load saved manga URLs when the page loads
+document.addEventListener("DOMContentLoaded", updateMangaList);
---
+++ manga_watch/manga.js
@@ -0,0 +1,99 @@
+function checkRead(id) {
+    let saved_manga = JSON.parse(localStorage.getItem("savedManga")) || {};
+    const { source, chapter_id, url } = getQueryParams();
+
+    checks = document.getElementsByClassName("read");
+    if (checks[checks.length-id].checked) {
+        // if box is checked
+        for (var i = checks.length-id; i < checks.length; i++) {
+            checks[i].checked = true;
+        }
+        saved_manga[url]["last_read"] = id;
+        localStorage.setItem("savedManga", JSON.stringify(saved_manga));
+    } else {
+        // if box is unchecked
+        for (var i = checks.length-id; i > 0; i--) {
+            checks[i].checked = false;
+        }
+        saved_manga[url]["last_read"] = id-1;
+        localStorage.setItem("savedManga", JSON.stringify(saved_manga));
+    }
+}
+
+function markRead(id) {
+    let saved_manga = JSON.parse(localStorage.getItem("savedManga")) || {};
+    const { source, chapter_id, url } = getQueryParams();
+
+    checks = document.getElementsByClassName("read");
+    // if box is checked
+    for (var i = checks.length-id; i < checks.length; i++) {
+        checks[i].checked = true;
+    }
+    saved_manga[url]["last_read"] = id;
+    localStorage.setItem("savedManga", JSON.stringify(saved_manga));
+}
+
+// Function to get URL parameters
+function getQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    return {
+        source: params.get("source"),
+        id: params.get("id"),
+        url: params.get("url")
+    };
+}
+
+// Function to fetch chapters from Flask backend
+function fetchChapters() {
+    const { source, id, url } = getQueryParams();
+    
+    if (!source || !id) {
+        console.error("Missing source or id in URL parameters.");
+        return;
+    }
+
+    fetch(`http://127.0.0.1:5000/get_chapters?source=${encodeURIComponent(source)}&id=${encodeURIComponent(id)}`)
+        .then(response => response.json())
+        .then(data => {
+            let saved_manga = JSON.parse(localStorage.getItem("savedManga")) || {};
+            last_read = saved_manga[url]["last_read"];
+            displayChapters(data, last_read);
+        })
+        .catch(error => console.error("Error fetching chapters:", error));
+}
+
+// Function to display chapters on the page
+function displayChapters(data, last_read) {
+    // fetch saved manga data
+    const container = document.getElementById("manga_table");
+    container.innerHTML = "<tr>" +
+    '<th width="10%">Chapter</th>' +
+    '<th width="80%">Title</th>' +
+    '<th width="10%">Read?</th>' +
+    '</tr>'; // Clear previous content
+
+    if (data.chapters && data.chapters.length > 0) {
+        data.chapters.forEach(chapter => {
+            let read = (parseInt(chapter['chapter_no']) <= parseInt(last_read))
+
+            const chapterRow = document.createElement("tr");
+            const chapterNo = document.createElement("td");
+            const chapterTitle = document.createElement("td");
+            const lastRead = document.createElement("td");
+
+            chapterRow.id = chapter['chapter_no'];
+            chapterNo.innerHTML = chapter['chapter_no'];
+            chapterTitle.innerHTML = `<a href="${chapter['chapter_url']}" onclick="markRead(this.parentElement.parentElement.id)" target="_blank">${chapter['chapter_title']}</a>`;
+            lastRead.innerHTML = `<input class="read" onclick="checkRead(this.parentElement.parentElement.id)" type="checkbox" ${read? "checked":""}>`;
+            chapterRow.appendChild(chapterNo);
+            chapterRow.appendChild(chapterTitle);
+            chapterRow.appendChild(lastRead);
+            container.appendChild(chapterRow);
+        });
+    } else {
+        container.innerHTML = "<p>No chapters found.</p>";
+    }
+}
+
+// Run when page loads
+window.onload = fetchChapters;
